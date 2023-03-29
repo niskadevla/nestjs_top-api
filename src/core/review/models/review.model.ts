51305bf04424d8b81c685ad62272acc950e53983
niskadevla <niskadevla@gmail.com>
2023-03-29 21:17:10 +0200

    feat: add e2e test for review

--- src/core/review/models/review.model.ts
+++ src/core/review/models/review.model.ts
@@ -1,6 +1,6 @@
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
-import { prop } from '@typegoose/typegoose';
-import { Types } from 'mongoose';
+import { mongoose, prop } from '@typegoose/typegoose';
+type ObjectId = mongoose.Schema.Types.ObjectId;
 
 export interface ReviewModel extends Base {}
 export class ReviewModel extends TimeStamps {
@@ -17,5 +17,5 @@ export class ReviewModel extends TimeStamps {
   rating: number;
 
   @prop()
-  productId: Types.ObjectId;
+  productId: ObjectId;
 }
