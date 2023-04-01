696878bed1c76a1fcbf65558bf58d72904248dbc
Denys Bielishev <48553334+niskadevla@users.noreply.github.com>
2023-04-01 19:01:59 +0300

    Feature/add validators (#4)
    * feat: add validator for create-review
    
    * feat: add validator for auth

--- src/core/auth/dto/auth.dto.ts
+++ src/core/auth/dto/auth.dto.ts
@@ -1,4 +1,19 @@
+import { IsDefined, IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+const strongPassword = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{6,})');
+const errorMessage =
+  'Password is too weak. It must contain at least 1 lowercase alphabetical character, at least 1 uppercase alphabetical character, at least 1 numeric character, and be six characters or longer';
+
 export class AuthDto {
+  @IsString()
+  @IsDefined()
   login: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(strongPassword, {
+    message: errorMessage,
+  })
   password: string;
 }
--- src/core/review/dto/create-review.dto.ts
+++ src/core/review/dto/create-review.dto.ts
@@ -1,7 +1,25 @@
+import { IsNotEmpty, IsNumber, IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString()
+  @MinLength(2)
   name: string;
+
+  @IsString()
+  @MinLength(2)
+  @MaxLength(100)
   title: string;
+
+  @IsString()
+  @MaxLength(500)
   description: string;
+
+  @Max(5)
+  @Min(1, { message: 'Rating must not be less 1' })
+  @IsNumber()
   rating: number;
+
+  @IsString()
+  @IsNotEmpty()
   productId: string;
 }
